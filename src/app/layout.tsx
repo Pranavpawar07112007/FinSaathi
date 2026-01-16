a03f050c465010f50d9e87be747b58a517328597
Pranav Pawar <pranav07112007@gmail.com>
2026-01-16 07:20:08 +0000

    good one man its looking hot. can you add a blend of 4-5 colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,10 @@ export default function RootLayout({
           aria-hidden="true"
         >
           <div className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[64rem] -translate-x-1/2 rounded-full bg-primary/10 blur-3xl" />
-          <div className="absolute left-1/2 top-1/2 h-[48rem] w-[48rem] -translate-x-1/4 -translate-y-1/2 rounded-full bg-accent/20 blur-3xl" />
+          <div className="absolute left-1/2 top-1/2 h-[48rem] w-[48rem] -translate-x-1/4 -translate-y-1/2 rounded-full bg-accent/10 blur-3xl" />
+          <div className="absolute -left-48 top-1/4 h-[56rem] w-[56rem] -translate-x-1/4 -translate-y-1/2 rounded-full bg-chart-3/10 blur-3xl" />
+          <div className="absolute -right-48 bottom-0 h-[56rem] w-[56rem] translate-x-1/4 translate-y-1/2 rounded-full bg-chart-4/10 blur-3xl" />
+          <div className="absolute left-1/2 bottom-1/4 h-[48rem] w-[48rem] -translate-x-1/2 translate-y-1/2 rounded-full bg-chart-5/10 blur-3xl" />
         </div>
         <ThemeProvider
           attribute="class"
